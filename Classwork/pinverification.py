d552e447d18059199ad572a50360d25bf249aa8d
Yuvraj singh <yuvrajsinghh1133@gmail.com>
2026-06-04 12:02:44 +0530

    Delete Classwork/pinverification.py

--- Classwork/pinverification.py
+++
@@ -1,9 +0,0 @@
-pin = 1234
-
-entered_pin = int(input("Enter PIN: "))
-
-while entered_pin != pin:
-    print("Incorrect PIN. Try Again.")
-    entered_pin = int(input("Enter PIN: "))
-
-print("Access Granted.")
