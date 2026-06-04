f2c87fe1f0fb28c40846c53dbafadc918055e400
Yuvraj singh <yuvrajsinghh1133@gmail.com>
2026-06-04 14:00:50 +0530

    Create Student Result Management System.py

---
+++ labwork/lab work 4th june/Student Result Management System.py
@@ -0,0 +1,27 @@
+  marks = []
+fail_count = 0
+
+for i in range(5):
+    m = int(input(f"Enter marks for subject {i+1}: "))
+    marks.append(m)
+    if m < 40:
+        fail_count += 1
+
+total = sum(marks)
+percentage = total / 5
+
+if percentage >= 90:
+    grade = "A+"
+elif percentage >= 75:
+    grade = "A"
+elif percentage >= 60:
+    grade = "B"
+elif percentage >= 40:
+    grade = "C"
+else:
+    grade = "Fail"
+
+print("Total Marks:", total)
+print("Percentage:", percentage)
+print("Grade:", grade)
+print("Subjects Failed:", fail_count)
