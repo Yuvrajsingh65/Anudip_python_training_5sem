093a5ad59fc10eacfeeee109c91f5049be1ca48b
Yuvraj singh <yuvrajsinghh1133@gmail.com>
2026-06-06 14:59:26 +0530

    Update warehouse_pro_insp.py

--- labwork/lab_work 6th june/warehouse_pro_insp.py
+++ labwork/lab_work 6th june/warehouse_pro_insp.py
@@ -6,51 +6,56 @@ products = [
     (101, "Pass"), 
     (102, "Fail"), 
     (103, "Pass"), 
-    (104, "Fail"),
+    (104, "Fail"), 
+    (105, "Pass") 
+] 
+Write a program to: 
+• Display failed product IDs.  
+• Count passed and failed products.  
+• Calculate pass percentage.  
+• Stop checking if 3 failures are found. 
 '''
 # Product ID and Quality Status
 products = [
     (101, "Pass"),
     (102, "Fail"),
     (103, "Pass"),
-    (104, "Fail")
+    (104, "Fail"),
+    (105, "Pass")
 ]
 
-# Count Passed and Failed Products
-
+# Variables to count pass and fail products
 pass_count = 0
 fail_count = 0
+
+print("Failed Product IDs:")
 
 # Check each product
 for pid, status in products:
 
+    # If product passed
     if status == "Pass":
         pass_count += 1
+
+    # If product failed
     else:
         fail_count += 1
-
-print("Passed Products =", pass_count)
-print("Failed Products =", fail_count)
-
-# Display Failed Product IDs
-
-print("\nFailed Product IDs:")
-
-for pid, status in products:
-
-    if status == "Fail":
         print(pid)
+
+        # Stop checking if 3 failures are found
+        if fail_count == 3:
+            print("3 failures found. Stopping inspection.")
+            break
+
+# Display Pass and Fail Counts
+print("\nPassed Products =", pass_count)
+print("Failed Products =", fail_count)
 
 # Calculate Pass Percentage
 
-total_products = len(products)
+total_products = pass_count + fail_count
 
 pass_percentage = (pass_count / total_products) * 100
 
-print("\nPass Percentage =", pass_percentage, "%")
-
-# Determine Overall Result
-if pass_percentage >= 50:
-    print("Overall Result : ACCEPTED")
-else:
-    print("Overall Result : REJECTED")
+print("Pass Percentage =", pass_percentage, "%")
+    
